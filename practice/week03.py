fe565cd30b8a301a2c9e20e53bea873b6335603e
zzingni <wldms001313@gmail.com>
2023-10-12 07:42:21 +0900

    v0.8 number

--- practice/week03.py
+++ practice/week03.py
@@ -1,5 +1,20 @@
 
 
+print(3 + 2) # 5
+print(3 * 2) # 6
+print(3 / 2) # 1.5 
+print(3 // 2) # 1
+print(3 % 2) # 1
+print(3 ** 2) # 9
+
+
+reg_number = "030101-3123123"
+
+print(reg_number[0:2]) # 연도출력
+print(reg_number[2:4]) # 월 출력
+print(reg_number[4:6]) # 일 출력
+print(reg_number[8]) # 성별 출력(인덱스)
+
 my_school = "inha"
 
 length = len(my_school)

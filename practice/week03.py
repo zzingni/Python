e4665555ec2a456bb946e52c7e1426291374d42b
zzingni <wldms001313@gmail.com>
2023-10-12 08:00:32 +0900

    v1.2 if_1

--- practice/week03.py
+++ practice/week03.py
@@ -1,5 +1,18 @@
 
 
+if True:
+    pass
+print("해야지")
+
+if True:
+    print("참")
+    print("true")
+    if 10 < 100:
+        print("맞네!")
+
+if False:
+    print("거짓")
+
 print(not True)
 print(not False)
 

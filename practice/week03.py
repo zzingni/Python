6c30e006a25e5c8a87d6b0e30598008c8b5c61a3
zzingni <wldms001313@gmail.com>
2023-10-12 07:26:31 +0900

    v0.6 average

--- practice/week03.py
+++ practice/week03.py
@@ -4,6 +4,6 @@ num1 = int(input("1st:"))
 num2 = int(input("2nd:"))
 num3 = int(input("3rd:"))
 
-avg = (num1 + num2 + num3) / 3
+avg = (num1 + num2 + num3)/3
 
 print(str(num1) + "," + str(num2) + "," + str(num3) + "의 평균은" + str(avg)[:4] + "입니다.")

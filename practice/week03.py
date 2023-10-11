a8c1951845c647fda88377b4f54251d4d12b8892
zzingni <wldms001313@gmail.com>
2023-10-12 07:14:11 +0900

    v0.5 indexint & slicing 2

--- practice/week03.py
+++ practice/week03.py
@@ -1,32 +1,6 @@
 
 
-mymajor = "컴퓨터정보"
-print(mymajor[0:3]) # 컴퓨터
-print(mymajor[1:3]) # 퓨터
-print(mymajor[2:3]) # 터
-print(mymajor[:3]) # 컴퓨터
-print(mymajor[3:5]) # 정보
-print(mymajor[:]) # 컴퓨터정보
-print(mymajor[0:5:2]) # 컴터보
-print(mymajor[0:5:1]) # 컴퓨터정보
-print(mymajor) # 컴퓨터정보 / 슬라이싱 해도 원본은 변경 안 됨.
+number = input("주민등록번호(예제:001212-3456789):")
 
-
-
-mymajor = "컴퓨터정보"
-# print(mymajor[-6])
-print(mymajor[-5])
-print(mymajor[-4])
-print(mymajor[-3])
-print(mymajor[-2])
-print(mymajor[-1])
-print(mymajor[0])
-print(mymajor[1])
-print(mymajor[2])
-print(mymajor[3])
-print(mymajor[4])
-
-print("=-" * 30)
-
-print(20+20)
-print("나이:" + "20")
+print("연월일:", number[0:6])
+print("성별:", number[7])

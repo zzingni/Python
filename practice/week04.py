9cb2b14a7e5ba34469a3603950a33ee8158a3720
zzingni <wldms001313@gmail.com>
2023-10-12 18:01:01 +0900

    v0.9 pop()

--- practice/week04.py
+++ practice/week04.py
@@ -1,31 +1,26 @@
 
+test = ["수박", "귤", "메론", "배", "딸기"]
 
-test_list = [1,2,3]
-test_list.append(5)
-test_list.append(8)
-print(test_list) # 1,2,3,5,8
+del test[4]
+print(test) # "수박", "귤", "메론", "배"
 
-test_list.insert(3, 7)
-print(test_list) # 1,2,3,7,5,8
+del test[0:2]
+print(test) # "메론", "배"
 
-test_list.insert(-19, 0)
-print(test_list) # 0,1,2,3,7,5,8
-# 음수 인덱스는 리스트의 끝에서부터 역으로 셈. 인덱스 범위 벗어나면 맨 앞에 0이 추가됨.
+test.append("망고")
+test.append("귤")
+test.append("블루베리")
+print(test) # "메론", "배", "망고", "귤", "블루베리"
 
-test_list.insert(100,200)
-print(test_list, len(test_list)) # 0,1,2,3,7,5,8,100
-# 100이면 맨 뒤에 있을 거라 생각하고 맨 뒤에 추가해줌.
+test.remove("귤") 
+print(test) # "메론", "배", "망고", "블루베리"
 
-test_list1 = [1,2,3]
-test_list2 = [3,4]
+p = test.pop() # "메론", "배", "망고"
+print(p, type(p)) # 출력 : 블루베리, str
+# 원본 리스트 : "메론" "배", "망고" / 원본 리스트에서는 빠지지만 p라는 변수에 값은 저장됨
+# 즉, pop()메소드는 값을 삭제하지 않고 어딘가에 보관하고 있는 것.
 
-print(1+1) # 2
-print("1" + "1") # 11
-print(test_list1 + test_list2) # 1,2,3,3,4
-print(test_list2 * 3) # 3,4,3,4,3,4
-print(test_list1) # 1,2,3 > 리스트 원본 변경 안 됨
-print(len(test_list1)) # 3
-
-test_list1 = [1,2,3]
-test_list2 = ["3","4"]
-print(test_list1 + test_list2) # 1, 2, 3, '3', '4' > 배열 내 요소의 타입이 동일하진 않지만 리스트 + 리스트 는 리스트의 합을 보여줌.
+test.pop() # "배"
+test.pop(0) # ""
+# 원래 스택 구조상 제일 뒤에 있는 걸 빼주는게 pop()인데 파이썬에서는 지정해서 빼줄
+print(test) # 출력x

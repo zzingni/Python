5aa322b98582ed2951058d659327843c504b2609
zzingni <wldms001313@gmail.com>
2023-10-12 18:49:05 +0900

    v0.3 datetime

--- practice/week05.py
+++ practice/week05.py
@@ -1,29 +1,19 @@
 
 
-b = "10 20-30 40-50,60"
-print("10" in b)
-print("11" in b)
-print("12" not in b)
+import datetime
 
-b = "10 20-30 40-50,60"
-c = b.split()
-print(c)
-print("=" * 30)
+now = datetime.datetime.now()
+
+print(now.year)
+print(now.month)
+print(now.day)
+
+print(now.hour)
+print(now.minute)
+print(now.second)
 
 
-a = "       Hello PYthon!        "
-
-print(a.find("o"))
-print(a.find("O"))
-print(a.rfind("o"))
-print()
-
-print(a.strip())
-print(f"[{a.strip()}]")
-print(f"[{a.lstrip()}]")
-print(f"[{a.rstrip()}]")
-print()
-
-print(a.upper())    # HELLO PYTHON!
-print(a.lower())    # hello python!
-print(a.title())    # Hello Python!
+if now.hour < 12:
+    print("오전")
+else:
+    print("오후")

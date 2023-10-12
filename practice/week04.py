f76155280fb0faa4a0a82b2238b133be3b3c68b7
zzingni <wldms001313@gmail.com>
2023-10-12 17:47:42 +0900

    v0.8 append(), insert()

--- practice/week04.py
+++ practice/week04.py
@@ -1,4 +1,21 @@
 
+
+test_list = [1,2,3]
+test_list.append(5)
+test_list.append(8)
+print(test_list) # 1,2,3,5,8
+
+test_list.insert(3, 7)
+print(test_list) # 1,2,3,7,5,8
+
+test_list.insert(-19, 0)
+print(test_list) # 0,1,2,3,7,5,8
+# 음수 인덱스는 리스트의 끝에서부터 역으로 셈. 인덱스 범위 벗어나면 맨 앞에 0이 추가됨.
+
+test_list.insert(100,200)
+print(test_list, len(test_list)) # 0,1,2,3,7,5,8,100
+# 100이면 맨 뒤에 있을 거라 생각하고 맨 뒤에 추가해줌.
+
 test_list1 = [1,2,3]
 test_list2 = [3,4]
 

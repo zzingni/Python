19d54739ba9eb850670b5904f09ea9a04d9fdb21
zzingni <wldms001313@gmail.com>
2023-10-12 17:27:51 +0900

    v0.7 list+list

--- practice/week04.py
+++ practice/week04.py
@@ -1,27 +1,14 @@
 
+test_list1 = [1,2,3]
+test_list2 = [3,4]
 
-stu_1 = [202344069, "김인하", "컴정", 20]
-stu_2 = [202344000, "이인하", "컴정", 20]
+print(1+1) # 2
+print("1" + "1") # 11
+print(test_list1 + test_list2) # 1,2,3,3,4
+print(test_list2 * 3) # 3,4,3,4,3,4
+print(test_list1) # 1,2,3 > 리스트 원본 변경 안 됨
+print(len(test_list1)) # 3
 
-print(stu_2[1][0]) # "이"
-print(str(stu_2[0])[:4])    # 2023
-
-a = ["hiimjiuen"]
-ac = a[:4]
-print(type(ac)) # 리스트의 슬라이싱 값은 리스트!!!!
-
-print("학번", type(stu_1[0]), stu_2[0]) # 인덱싱 값은 인덱싱 한 자료형에 따라 다름.
-print("이름", type(stu_1[1]), stu_2[1])
-
-test_list = list()
-print(test_list, type(test_list))
-
-test_list = []
-print(test_list, type(test_list))
-
-
-test_list = ["권지은", 25, 166.6]
-print(test_list)
-
-for data in test_list:
-    print(test_list) # test_list안의 개수만큼 for문 반복됨, 4번 출력.
+test_list1 = [1,2,3]
+test_list2 = ["3","4"]
+print(test_list1 + test_list2) # 1, 2, 3, '3', '4' > 배열 내 요소의 타입이 동일하진 않지만 리스트 + 리스트 는 리스트의 합을 보여줌.

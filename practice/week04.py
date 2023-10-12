dd5305f60f4b6821a39172dee4b99c2a86538e29
zzingni <wldms001313@gmail.com>
2023-10-12 09:25:42 +0900

    v0.1 type

---
+++ practice/week04.py
@@ -0,0 +1,16 @@
+
+
+string ="안녕하세요"
+string += "!"
+string += "!"
+print("string:", string)
+
+int_a = int(52)
+int_a1 = int("52")
+float_a = float(27.2)
+float_a1 = float("27.2")
+
+print(type(int_a), int_a)
+print(type(int_a1), int_a1)
+print(type(float_a), float_a)
+print(type(float_a1), float_a1)

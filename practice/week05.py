c47657d3b6b257509ee047e07a4d3b9c66917b45
zzingni <wldms001313@gmail.com>
2023-10-13 16:40:44 +0900

    v0.9 dict_4

--- practice/week05.py
+++ practice/week05.py
@@ -1,18 +1,21 @@
 
-
 infos = {
-    "202300001":{"name":"kim inha", "age":21},
-    "202300002":{"name":"choi comjung", "age":22, "major":"컴퓨터정보"}
+    "202344069":{"name":"kim inha","age":21},
+    "202344444":{"name":"choi comjung", "age":22, "major":"컴퓨터정보"}
 }
 
-number = input("학번:")
-# print(infos[number])
-
-info = infos.get(number, "학번이 없어요")
-print(info)
+for key, value in infos.items():
+    print(key, value)
 print()
 
-if number in infos: # keys() 메소드 쓰지 않아도 기본적으로 키 값에서 찾아줌
-    print(infos[number])
-else:
-    print("학번이 없습니다.")
+for key in infos.keys():
+    print(infos[key])   # 값들이 출력됨!!!!! 헷갈리지 말기 ㅠㅠㅠㅠㅠ 
+print()
+
+for key in infos.keys():
+    print(key)
+print()
+
+for value in infos.values():
+    print(value)
+print()

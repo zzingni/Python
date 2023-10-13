b6afde900f48077cdd35410d451a12ee679f8a14
zzingni <wldms001313@gmail.com>
2023-10-13 16:10:24 +0900

    v0.6 dict_1

--- practice/week05.py
+++ practice/week05.py
@@ -1,25 +1,47 @@
 
 
 
-list_of_list = [
-    [1,2,3],
-    [3,4,5,7],
-    [10,11]
-]
+a = "string"
+a[0] # s
 
-for items in list_of_list:
-    for item in items:
-        print(item, end=" ")
-    print()
+b = [1,2,3]
+b[0] # 1
 
-for items in list_of_list:
-    print(items) # [1,2,3] [3,4,5,7] [10,11]
+infos = {
+    "202300001":{"name" : "kim inha", "age" : 21},
+    "202300002":{"name" : "choi comjung", "age" : 22, "major":"컴퓨터정보"}
+}
 
-name = "권지은"
-scores = [22, 100, 30, 40]
+print(infos["202300002"]["major"])
 
-for ele in name:
-    print(ele, end="/") # 권/지/은
+infos = {1:["kim iniha", 21], 2:["choi comjung", 22]}
+infos[2] # ['choi comjung', 22]
+print(infos[2][1])
 
-for score in scores:
-    print(score)    # 22 100 30 40(줄바꿈)
+
+info_a = {"name" : "kim inha", "age" : 21}
+info_b = {"name" : "choi comjung", "age" : 22}
+
+# print(info_a[0]) # 키 에러 / 인덱스로 접근 불가 / 딕셔너리는 순번 없음
+# print(info_a[name]) # 네임 에러 / name 이라는 변수 없음
+# print(info_a["이름"]) # 키 에러
+
+print(info_a['name'])
+print(info_a['age'])
+
+
+
+test_dict = {
+    "one" : 1,
+    "one" : 5, # key 값이 동일하면 덮어씌어짐.
+    "two" : 2,
+    "three" : 3
+}
+
+print(test_dict)
+
+test_dict = dict()
+print(test_dict, type(test_dict))
+
+test_dict = {}
+print(test_dict, type(test_dict))

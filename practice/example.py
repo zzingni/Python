8fb5777daf661e69c0b20f1bd69633b00e6f9ae9
zzingni <wldms001313@gmail.com>
2023-10-19 00:49:00 +0900

    p187 Chinese zodiac sign

--- practice/example.py
+++ practice/example.py
@@ -1,7 +1,15 @@
 
 
-a = float(input("밑변의 길이를 입력해주세요: "))
-b = float(input("높이의 길이를 입력해주세요: "))
+str_input = input("태어난 해를 입력해 주세요> ")
+birth_year = int(str_input)
+birth = birth_year % 12
 
-c = ((a ** 2) + (b ** 2)) ** (0.5)
-print(f"빗변의 길이는 {c}입니다.")
+ddi = {0:"원숭이", 1:"닭", 2:"개", 3:"돼지", 4:"쥐", 5:"소", 6:"범", 7:"토끼", 8:"용", 9:"뱀", 10:"말", 11:"양"}
+
+
+for i in ddi:
+    if birth == i:
+        print(f"{ddi[i]}띠 입니다.")
+        break
+    else:
+        continue

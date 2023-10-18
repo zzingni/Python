e11e71d79edd418fa100c0f9ce728adbe91566d8
zzingni <wldms001313@gmail.com>
2023-10-19 02:30:10 +0900

    p215 list ex2

--- practice/example.py
+++ practice/example.py
@@ -1,8 +1,9 @@
 
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-output = [[],[],[]]
-print(len(output))
-for number in numbers:
-    output[(number-1) % len(output)].append(number)
 
-print(output)
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+for i in range(0, len(numbers) // 2):
+    j = i + (i + 1)
+    print(f"i = {i}, j = {j}")
+    numbers[j] = numbers[j] ** 2
+print(numbers)

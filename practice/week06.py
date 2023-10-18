0d452e573e888c6f9415c67211e7363ccf3f8357
zzingni <wldms001313@gmail.com>
2023-10-18 18:06:48 +0900

    v0.2 range in for

--- practice/week06.py
+++ practice/week06.py
@@ -1,5 +1,56 @@
 
 
+
+fruits = ["딸기", "귤", "키위", "복숭아"]
+
+scores = [10,20,30,20]
+
+print(max(scores))
+print(min(scores))
+
+max_score = scores[0]
+for i in range(1, len(scores)):
+    if max_score < scores[i]:
+        max_score = scores[i]
+print(max_score)
+
+min_score = scores[0]
+for i in range(1, len(scores)):
+    if min_score > scores[i]:
+        min_score = scores[i]
+print(min_score)
+
+
+
+for i in reversed(range(len(fruits))):
+    print(f"{i+1} 순위 {fruits[i]}")
+print() # 순위 내림차순
+
+for i in range(len(fruits)):
+    print(f"{i+1} 순위 {fruits[i]}")
+print()
+
+# for i in range(5):
+#    print(f"{i+1} 순위 {fruits[i]}")
+# print()  # list 요소 개수와 i 개수 일치하지 않아 에러 생김
+
+
+for fruit in fruits:
+    print(fruit)
+print()
+
+for i in range(5,10,3):
+    print(i)
+
+for i in range(1,10):
+    print(i)
+
+for i in range(10):
+    print(i)
+
+for i in [0,1,2,3,4]:
+    print(i)
+
 range_test4 = range(10)
 range_test5 = range(2,10)
 range_test6 = range(2, 10, 3)

60d2b45157c569c0bc864192ba231784ed2cfa05
zzingni <wldms001313@gmail.com>
2023-10-18 19:49:04 +0900

    v0.4 reversed()

--- practice/week07.py
+++ practice/week07.py
@@ -1,71 +1,36 @@
 
 
-ip_addr = [127, 0, 0, 1]
-ip_addr_new = [str(ip) for ip in ip_addr]
-ip_addr_new = ".".join(ip_addr_new) 
-print(ip_addr_new, type(ip_addr_new))
-# join()으로 합치면 타입이 string이 됨!!
-
-
-array = []
-for i in range(0,20,2):
-    if i % 4:
-        array.append(i * i) 
-print(array)
-print()
-
-array = [i * i for i in range(0,20,2) if i % 4]
-print(array)
-
-
-
-array = []
-for i in range(0, 20, 2):
-    array.append(i * i)
-print(array)
-print()
-
-array = [ i * i for i in range(0, 20, 2)]
-print(array)
-
-
-fruits = ["딸기", "사과", "배"]
-
-print(", ".join(fruits))
-# join 메서드로 연결
-
-my_fev = ""
-for f in fruits:
-    if len(my_fev) > 0:
-        my_fev += ","
-    my_fev += f
-
-print(my_fev)
-print()
-# 함수로 연결
-
-example = {1:"one", 2:"two", 5:"five"}
-
-
-for k in example:
-    print(k)
-
-for k in example.keys():
-    print(k)
-
-for v in example.values():
-    print(v)
-
-for k, v in example.items():
-    print(k,v)
-
-
 scores = [100, 45, 80, 0, 23]
+# scores_new = reversed(scores) >>>> 에러!!!!
+scores_new = list(reversed(scores))
+print(scores_new)
+print()
+print(reversed(scores))
+print(list(reversed(scores)))
+print(scores)
 
-for i, score in enumerate("권지은"):
-    print(f"{i+1}번 과목 : {score}")
+# reversed랑 soted는 원본 리스트를 변경하지 않음
+# reversed는 이터레이터 객체가 !!!반환!!! 되기 때문에 새로운 list 생성필요
+# 따라서, 리스트로 변환해줘야함!!!!!!!!!!!
 
-for i, score in enumerate(scores):
-    print(f"{i+1}번 과목 : {score}")
+print(sorted(scores))
+print(list(sorted(scores)))
+print(scores)
 
-# enumerate : 반복 가능한 객체를 받아서 해당 객체의 각 요소와 해당 요소의 인덱스를 반환
+# sorted는 revered와 달리 순회 가능한 객체를 정렬한 새로운 리스트 반환!
+# 따라서 에러를 발생시키진 않음!!!!!!!
+
+a = reversed(scores)
+for r in a:
+    print("1st ", r) # reversed()는 한 번만 실행되며 없어짐. 1회용으로 쓸 수 있음.
+for r in a: # 2nd 출력 안 됨
+    print("2nd ",r)
+
+for r in reversed(scores):
+    print(r)
+
+a = list(reversed(scores))
+for r in a:
+    print("1st ", r)
+for r in a:
+    print("2nd ", r)

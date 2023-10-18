3e0c7297b517fd654f43be493bf3aacaf6982119
zzingni <wldms001313@gmail.com>
2023-10-18 18:40:25 +0900

    v0.3 while

--- practice/week06.py
+++ practice/week06.py
@@ -1,76 +1,67 @@
 
 
+infos = {
+    "123" : ["김인하", 22],
+    "124" : ["김인하", 21]
+}
 
-fruits = ["딸기", "귤", "키위", "복숭아"]
+print("학생 검색 시스템")
 
-scores = [10,20,30,20]
+while True:
+    number = input("학번: ")
+    if number in infos:
+        print(f"이름:{infos[number][0]}")
+    else:
+        print("그런 애 없다.")
 
-print(max(scores))
-print(min(scores))
+    yesorno = input("계속할래?(y,n) ")
+    if yesorno.upper() != "Y":
+        break
+# 학번 일치 불일치 상관없이 한 차례 검사 후 계속할지 물어보기
 
-max_score = scores[0]
-for i in range(1, len(scores)):
-    if max_score < scores[i]:
-        max_score = scores[i]
-print(max_score)
+while True:
+    number = input("학번: ")
+    if number in infos:
+        print(f"이름:{infos[number][0]}")
+        break
+    else:
+        yesorno = input("계속할래>(y,n) ")
+        if yesorno.upper() == "Y":
+            continue
+        else:
+            break
+# 학번이 일치하지 않을 때 계속할지 물어보기
 
-min_score = scores[0]
-for i in range(1, len(scores)):
-    if min_score > scores[i]:
-        min_score = scores[i]
-print(min_score)
+while True:
+    number = input("학번: ")
+    if not number in infos:
+        continue
+    print(f"이름:{infos[number][0]}")
+    break
+# 학번이 일치하지 않으면 continue로 계속 조건검사
+# 일치하면 출력하고 break
 
 
+while True:
+    number = input("학번: ")
+    if number in infos.keys():
+        print(f"이름:{infos[number][0]}")
+        break
+    else:
+        pass
+    print("학번 다시 입력해야해")
+# 학번이 일치하면 이름 출력하고 종료
+# 불일치시 계속 학번 입력받기
 
-for i in reversed(range(len(fruits))):
+fruits = ["딸기", "굴", "키위", "키위", "키위", "복숭아"]
+
+target = "키위"
+while target in fruits: # in 연산자
+    fruits.remove(target)
+print(fruits)
+
+i = 0
+while i < len(fruits):
     print(f"{i+1} 순위 {fruits[i]}")
-print() # 순위 내림차순
-
-for i in range(len(fruits)):
-    print(f"{i+1} 순위 {fruits[i]}")
+    i += 1 # python에서는 i++ 안됨.
 print()
-
-# for i in range(5):
-#    print(f"{i+1} 순위 {fruits[i]}")
-# print()  # list 요소 개수와 i 개수 일치하지 않아 에러 생김
-
-
-for fruit in fruits:
-    print(fruit)
-print()
-
-for i in range(5,10,3):
-    print(i)
-
-for i in range(1,10):
-    print(i)
-
-for i in range(10):
-    print(i)
-
-for i in [0,1,2,3,4]:
-    print(i)
-
-range_test4 = range(10)
-range_test5 = range(2,10)
-range_test6 = range(2, 10, 3)
-print(range_test4)
-print(range_test5)
-print(range_test6)
-
-
-
-range_test1 = list(range(10))
-range_test2 = list(range(2,10))
-range_test3 = list(range(2,10,3))
-
-print(range_test1)
-print(range_test2)
-print(range_test3)
-
-# list = [1,2,3]
-# a = list(["Aaaa"])    # 리스트 생성자 사용
-# print(a)
-
-# range = range(10)
-# print(range. type(range))

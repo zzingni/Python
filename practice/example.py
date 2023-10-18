bc8ee236ba371f7ea02cb8cd3c9f6ff52b2e5d27
zzingni <wldms001313@gmail.com>
2023-10-19 03:17:07 +0900

    numbers in dict

--- practice/example.py
+++ practice/example.py
@@ -1,11 +1,10 @@
 
 
-pets = [
-    {"name" : "구름", "age" : 5},
-    {"name" : "초코", "age" : 3},
-    {"name" : "아지", "age" : 1},
-    {"name" : "호랑이", "age" : 1}
-]
+numbers = [1,2,6,8,4,3,2,1,1,3,7,8,9,5,3,3,2,2,2,3,5,6]
+counter = {}
 
-for value in pets:
-    print(f"{value['name']} {value['age']}살")
+for number in numbers:
+    if number not in counter:
+        counter[number] = 0
+    counter[number] += 1 # key값 생성 후 그 값도 +1 해줘야함. else 말고 if 밖에서 +=1 필요!
+print(counter)

bb47eeb5f6c85e5cdd060f129a8f7595a914fe58
zzingni <wldms001313@gmail.com>
2023-10-18 17:43:30 +0900

    0.1 range()

---
+++ practice/week06.py
@@ -0,0 +1,25 @@
+
+
+range_test4 = range(10)
+range_test5 = range(2,10)
+range_test6 = range(2, 10, 3)
+print(range_test4)
+print(range_test5)
+print(range_test6)
+
+
+
+range_test1 = list(range(10))
+range_test2 = list(range(2,10))
+range_test3 = list(range(2,10,3))
+
+print(range_test1)
+print(range_test2)
+print(range_test3)
+
+# list = [1,2,3]
+# a = list(["Aaaa"])    # 리스트 생성자 사용
+# print(a)
+
+# range = range(10)
+# print(range. type(range))

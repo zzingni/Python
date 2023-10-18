6c2c349bceeb97bccb132dfbb257d60414ec9849
zzingni <wldms001313@gmail.com>
2023-10-19 01:04:42 +0900

    p188 simple conversation

--- practice/example.py
+++ practice/example.py
@@ -1,15 +1,35 @@
 
 
-str_input = input("태어난 해를 입력해 주세요> ")
-birth_year = int(str_input)
-birth = birth_year % 12
+# 간단한 대화 프로그램
 
-ddi = {0:"원숭이", 1:"닭", 2:"개", 3:"돼지", 4:"쥐", 5:"소", 6:"범", 7:"토끼", 8:"용", 9:"뱀", 10:"말", 11:"양"}
+import datetime
 
+now = datetime.datetime.now()
+hour = now.hour
 
-for i in ddi:
-    if birth == i:
-        print(f"{ddi[i]}띠 입니다.")
-        break
+while True:
+    user = input("입력: ")
+    if "안녕" in user:
+        print("안녕하세요.")
+        answer = input("계속 하시겠습니까?(y,n) ")
+        if answer.upper() == "Y":
+            continue
+        else:
+            break
+    elif "몇 시" in user:
+        print(f"지금은 {hour}시입니다.") # print(f{user}) > 오류. f-string은 문자열과 함께 사용해야 쓸 수 없음. 변수명만 쓸 때는 오류.
+        answer = input("계속 하시겠습니까?(y,n) ")
+        if answer.upper() == "Y":
+            continue
+        else:
+            break
     else:
-        continue
+        print(user)
+        answer = input("계속 하시겠습니까?(y,n) ")
+        if answer.upper() == "Y":
+            continue
+        else:
+            break
+
+
+        

301587087871bf84fc193269eba804c487e354ab
zzingni <wldms001313@gmail.com>
2023-10-19 09:42:10 +0900

    test code_3

--- practice/example.py
+++ practice/example.py
@@ -1,10 +1,10 @@
 
+limit = 10000
+i = 1
 
-key_list = ["name", "hp", "mp", "level"]
-value_list = ["기사", 200, 30, 5]
-character = {}
+sum_value = 0
 
-for i, key in enumerate(key_list):
-    character[key] = value_list[i]
-print(character)
-    
+while sum_value <= 10000:
+    sum_value += i
+    i += 1
+print(f"{i-1}를 더할 때 {limit}를 넘으며 그때의 값은 {sum_value}입니다.")
